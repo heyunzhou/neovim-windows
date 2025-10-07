8e8365ad72b314a07ccc9138fd57f24bad97b7a7
heyunzhou <heyunzhou312@gmail.com>
2025-10-07 23:38:42 +0900

    add pwsh profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\iterm2.omp.json" | Invoke-Expression
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# enable zoxide
+zoxide init powershell | Out-String | Invoke-Expression
+
+# fzf setting
+$env:FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"
+Import-Module PSFzf
+Import-Module PSFzfHistory
+Set-PSReadLineKeyHandler -Chord Ctrl+r -ScriptBlock { 
+    Invoke-FzfPsReadlineHandlerHistory
+}
+
+
+# alias
+Set-Alias vim nvim
+
+function zf {
+    z $(fd -t d . | fzf)
+}
