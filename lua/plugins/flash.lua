aa49a13c7f9ce75fbc86f67ede5b814d1217fa64
heyunzhou <heyunzhou312@gmail.com>
2025-10-13 13:20:52 +0900

    fix flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,7 +1,16 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy", -- 延迟加载（启动更快）
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
   -- 可选的快捷键绑定
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash jump" },
