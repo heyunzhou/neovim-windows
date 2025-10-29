3976d2899346d4ac7063dfedc81c458b6057e5c9
heyunzhou <heyunzhou312@gmail.com>
2025-10-29 19:59:51 +0900

    update flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -15,5 +15,7 @@ return {
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash jump" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
 }
